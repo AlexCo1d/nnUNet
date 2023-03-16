6dd91391bfb07cddb0a152a96ca170cf18c6c6c3
alexyhzou <alexyhzou@qq.com>
2023-03-16 16:11:14 +0800

    更新原始数据的预处理文件

--- My_task1_preprocess_raw_data.py
+++ My_task1_preprocess_raw_data.py
@@ -19,44 +19,46 @@ labelsTr = os.path.join(data_path, "labelsTr")
 labelsTs = os.path.join(data_path, "labelsTs")
 random_num = 100  # 设置随机选测试图像数目
 
-# 将图片统一编号
 image_list = os.listdir(image_path)
 label_list = os.listdir(label_path)
 
+# 随机采样分离出测试图片
 test_list = random.sample(image_list, random_num)
 # save test image to imagesTs和labelsTs
-for index,file_name in enumerate(test_list):
+for index, file_name in enumerate(test_list):
     # shutil.copy(os.path.join(image_path, file_name),os.path.join(imagesTs,'Test_Cervical_'+str(index)+'.jpg'))
     cur_image = np.asarray(np.array(Image.open(os.path.join(image_path, file_name)).convert('L')))
     w, h = cur_image.shape
     cur_image = cur_image.reshape(1, w, h)
     cur_image_nii = sitk.GetImageFromArray(cur_image)
     cur_image_name = 'Test_Cervical_' + str(index) + '_0000.nii.gz'
-    sitk.WriteImage(cur_image_nii,os.path.join(imagesTs,cur_image_name))
+    sitk.WriteImage(cur_image_nii, os.path.join(imagesTs, cur_image_name))
 
-    shutil.copy(os.path.join(label_path, file_name.replace(".jpg",".png")),os.path.join(labelsTs,'Test_Cervical_'+str(index)+'.png'))
+    shutil.copy(os.path.join(label_path, file_name.replace(".jpg", ".png")),
+                os.path.join(labelsTs, 'Test_Cervical_' + str(index) + '.png'))
 
 # 分理出需要训练的图片集
 train_list = list(set(image_list) - set(test_list))  # end with jpg
 
 for index, file_name in enumerate(train_list):
-
     # 读取文件并将其转为array
     cur_image = np.asarray(np.array(Image.open(os.path.join(image_path, file_name)).convert('L')))
     cur_label = np.asarray(np.array(Image.open(os.path.join(label_path, file_name.replace(".jpg", '.png')))))
     w, h = cur_image.shape
     cur_image = cur_image.reshape(1, w, h)
     cur_label = cur_label.reshape(1, w, h)
-    cur_image_name='Cervical_'+str(index)+'_0000.nii.gz'
-    cur_label_name='Cervical_'+str(index)+'.nii.gz'
+    cur_image_name = 'Cervical_' + str(index) + '_0000.nii.gz'
+    cur_label_name = 'Cervical_' + str(index) + '.nii.gz'
 
     # 转化为ntfi格式存储进各自需要的路径
-    cur_image_nii=sitk.GetImageFromArray(cur_image)
-    cur_label_nii=sitk.GetImageFromArray(cur_label)
+    cur_image_nii = sitk.GetImageFromArray(cur_image)
+    cur_label_nii = sitk.GetImageFromArray(cur_label)
 
-    sitk.WriteImage(cur_image_nii,os.path.join(imagesTr,cur_image_name))
-    sitk.WriteImage(cur_label_nii,os.path.join(labelsTr,cur_label_name))
+    sitk.WriteImage(cur_image_nii, os.path.join(imagesTr, cur_image_name))
+    sitk.WriteImage(cur_label_nii, os.path.join(labelsTr, cur_label_name))
 
 # generate json file
-generate_dataset_json(output_file=os.path.join(data_path,"dataset.json",), imagesTr_dir=imagesTr, imagesTs_dir=imagesTs,modalities=('gray')
-                      ,labels={0:'background',128:'tumor'},dataset_name='Task666_CervicalTumor',license='hands_off')
+generate_dataset_json(output_file=os.path.join(data_path, "dataset.json", ), imagesTr_dir=imagesTr,
+                      imagesTs_dir=imagesTs, modalities=('gray',)
+                      , labels={0: 'background', 128: 'tumor'}, dataset_name='Task666_CervicalTumor',
+                      license='hands_off')
